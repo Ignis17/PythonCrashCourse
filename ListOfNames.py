d89a82a92286a1044b937872c37e1ce156316712
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-09-29 21:07:51 -0400

    New python files

---
+++ ListOfNames.py
@@ -0,0 +1,5 @@
+names=["John","Alex","Eva","James","Barry","Carla","Vinnille","Ziggy","Gloria","Seth","Wesley","Christopher","Maria","Yamilet","Rosa","Jacqueline","Arfanne","Francis","Gladis"]
+names.sort()
+for name in names:
+    if name >= "Maria":
+        print(name)
---
+++ SmallestNumber.py
@@ -0,0 +1,11 @@
+def SmallestNumber(x,y,z):
+    least = x
+
+    if y < least:
+        least = y
+    if z < least:
+        least = z
+    return least
+
+result = SmallestNumber(3,65,1)
+print "The smallest number is: ",result
