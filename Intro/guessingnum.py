153cadb89826b350548a20f7e8aa8e4bfe0fec4d
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-26 19:39:22 -0400

    Intro to PythonCrashCourse

---
+++ Intro/guessingnum.py
@@ -0,0 +1,19 @@
+import random
+
+answer = ""
+guess = ""
+
+num = random.randint(0,100)
+guess = int(input("\nI'm thinking of a number! Try to guess the number I'm thinking of: "))
+while(answer != "no"):
+    if(num == guess):
+        answer = input("\nThat's it! Would you like to play again? (yes/no) ")
+        if(answer == "yes"):
+            num = random.randint(0,100)
+            guess = int(input("\nI'm thinking of a number! Try to guess the number I'm thinking of: "))
+        elif(answer == "no"):
+            print("Thanks for playing!")
+    elif(guess < num):
+        guess = int(input("Too low! Guess again: "))
+    elif(guess > num):
+        guess = int(input("Too high! Guess again: "))
---
+++ Intro/hello.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print("Mi nombre es Joel.")
