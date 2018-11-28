bd95f297db0def659522cd3b8fd1531c20dde575
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-11-27 21:48:24 -0500

    New files

---
+++ Chapter4/Animals.py
@@ -0,0 +1,5 @@
+animals = ["horse", "dog", "donkey", "cat", "turtle"]
+
+for animal in animals:
+    print("A " + animal + " would make a great pet.\n")
+print("Any of these animals would make a great pet!")
---
+++ Chapter4/Pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ["cheese", "pepperoni", "sausage"]
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizza.\n")
+print("I really love pizza!")
