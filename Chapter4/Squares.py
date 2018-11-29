f02dfa4262cdf037bf8ea04969a34a1307fd380c
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-11-29 16:43:47 -0500

    new squares.py file

---
+++ Chapter4/Squares.py
@@ -0,0 +1,11 @@
+squares = []
+
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+print(squares)
+
+### NOTE ###
+# The examples in this section use short lists of numbers in order to fit
+# easily on the page. They would work just as well if your list contained
+# a million or more numbers.
