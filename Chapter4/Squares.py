9f7ad57b3d9dd41c9a8c9ecdf3f2fffe3743ca8d
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-11-30 02:48:04 -0500

    Made changes to Squares.py

--- Chapter4/Squares.py
+++ Chapter4/Squares.py
@@ -3,7 +3,7 @@ squares = []
 for value in range(1, 11):
     square = value**2
     squares.append(square)
-print(squares)
+print("Powers of 2 from 1-10:\n",*squares)
 
 ### NOTE ###
 # The examples in this section use short lists of numbers in order to fit
